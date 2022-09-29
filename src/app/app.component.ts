915bbea7878afbfc1bc6dd3ebcc927d7eb2d248c
Harsh <harshdwivedi28@yahoo.in>
2022-09-29 18:46:55 +0530

    My First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My-App';
+  title = 'My Title';
+  count=0;
+
+  listOfNumbers=["Audi", "BMW", "Prime"];
+  listOfPL=["C++", "Java", "Python"];
+
+  increment = () => {
+    this.count++;
+  }
+
+  decrement = () => {
+    this.count--;
+  }
+
+  number1=10;
+  number2=11;
+  
+
+  Add = () => {
+    let myContainer = document.getElementById('A') as HTMLInputElement;
+    myContainer.value = String(this.number1+this.number2);
+  }
 }
